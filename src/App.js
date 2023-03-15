19c02bd0264f24bb88b6d2f52dcfef9d959efe65
Erik Pulatov <epulatov@gmail.com>
2023-03-15 16:28:15 -0500

    Complete HW1 and HW2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+const Header = (props) => {
+  return (
+    <div>
+      <h1> Course: {props.name}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.partName}: {props.partEx}</p>
+      
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Total number of exercises: {props.totalNumber}</p>
+    </div>
+  )
+}
 
 function App() {
+  const courseName = 'React';
+  const part1 = 'React Basics';
+  const part2 = 'React Advanced';
+  const part3 = 'Project';
+  const ex1 = 4;
+  const ex2 = 5;
+  const ex3 = 6;
+  const total = ex1 + ex2 + ex3;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header name={courseName}/>
+      <Content partName={part1} partEx={ex1}/>
+      <Content partName={part2} partEx={ex2}/>
+      <Content partName={part3} partEx={ex3}/>
+      <Total totalNumber={total} />
     </div>
   );
 }
